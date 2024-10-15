d03b744d34ec18f66b0f73bb80a822c593dac0ae
Rizki Maulana Citra <rmaulana.citra@gmail.com>
2024-10-15 12:01:59 +0700

    search doctor

--- src/app/(app)/doctor/list/page.tsx
+++ src/app/(app)/doctor/list/page.tsx
@@ -2,9 +2,14 @@ import { Heading } from "@/components/ui/heading";
 import { Fragment } from "react";
 import { DoctorList, DoctorListAddButton } from "@/features/doctor/list";
 import { getDoctors } from "@/database/query/doctor";
+import { F, O, pipe } from "@mobily/ts-belt";
 
-export default async function DoctorListPage() {
-  let doctors = await getDoctors();
+export default async function DoctorListPage(props: PageProps) {
+  let search = pipe(
+    O.fromNullable(props.searchParams?.q),
+    O.mapWithDefault("", F.identity)
+  );
+  let doctors = await getDoctors(search);
 
   return (
     <Fragment>
